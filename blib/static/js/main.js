5396f42cdcfd388513916af85d53d9eec6e8d99b
shisinbin <69313410+shisinbin@users.noreply.github.com>
2025-12-16 02:01:20 +0000

    Refactor alerts: replace jQuery with vanilla JS

--- blib/static/js/main.js
+++ blib/static/js/main.js
@@ -1,3 +1,11 @@
-setTimeout(function () {
-  $('#message').fadeOut('slow');
+// setTimeout(function () {
+//   $('.js-alert').fadeOut('slow');
+// }, 3000);
+
+setTimeout(() => {
+  document.querySelectorAll('.js-alert').forEach((alert) => {
+    alert.style.transition = 'opacity 0.5s';
+    alert.style.opacity = '0';
+    setTimeout(() => alert.remove(), 500);
+  });
 }, 3000);
