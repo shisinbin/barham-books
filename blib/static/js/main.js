646cff6a0f990d53d8a4cce5e2c81f9469ce9b1f
shisinbin <69313410+shisinbin@users.noreply.github.com>
2025-12-16 01:47:37 +0000

    Refactor accordion: replace jQuery-UI version with basic Boostrap one, move bootstrap-select stuff to appropriate pages

--- blib/static/js/main.js
+++ blib/static/js/main.js
@@ -1,25 +1,3 @@
 setTimeout(function () {
   $('#message').fadeOut('slow');
 }, 3000);
-
-// $(function () {
-//   $('#keywords').autocomplete({
-//     source: function (request, response) {
-//       $.ajax({
-//         url: "{% url 'autocomplete_books' %}",
-//         data: {
-//           term: request.term,
-//         },
-//         success: function (data) {
-//           response(data);
-//         },
-//       });
-//     },
-//     minLength: 3, // Minimum length for autocomplete to start
-//     select: function (event, ui) {
-//       if (ui.item.url) {
-//         window.location.href = ui.item.url; // Redirect to the book's URL on selection
-//       }
-//     },
-//   });
-// });
