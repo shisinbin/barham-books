c3496d64bf4efe3bf92c5046fdf85b5878843bdb
shisinbin <69313410+shisinbin@users.noreply.github.com>
2025-12-18 01:44:17 +0000

    Create a new index.html boilerplate set-up

--- pages/views.py
+++ pages/views.py
@@ -6,19 +6,20 @@ from blog.models import Post
 
 def index(request):
 	# redirecting home page to books
-	if 1 == 1:
-		return redirect('books')
-	else:
-		# books = Book.objects.filter(is_featured=True)[:3]
-		latest_posts = Post.published.all()[:3]
+	# if 1 == 1:
+	# 	return redirect('books')
+	# else:
+	# 	# books = Book.objects.filter(is_featured=True)[:3]
+	# 	latest_posts = Post.published.all()[:3]
 
-		context = {
-			#'books': books,
-			'latest_posts': latest_posts,
-			'language_choices': language_choices,
-		}
+	# 	context = {
+	# 		#'books': books,
+	# 		'latest_posts': latest_posts,
+	# 		'language_choices': language_choices,
+	# 	}
 
-		return render(request, 'pages/index.html', context)
+	# 	return render(request, 'pages/index.html', context)
+	return render(request, 'pages/index.html')
 
 def about(request):
 	return render(request, 'pages/about.html')
