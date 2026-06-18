9f830c8813cc13d851aceecbe9d10fc419b636d7
shisinbin <69313410+shisinbin@users.noreply.github.com>
2026-06-18 11:39:40 +0100

    Remove/comment out remaining LMS Record and Reservation references

--- reservations/views.py
+++ reservations/views.py
@@ -1,61 +1,61 @@
-from django.shortcuts import render, redirect
-from django.utils import timezone
-from datetime import date
-from django.contrib import messages
-from records2.models import Record
-from books.models import BookInstance2, Book
-from .models import Reservation
-from django.core.mail import send_mail
+# from django.shortcuts import render, redirect
+# from django.utils import timezone
+# from datetime import date
+# from django.contrib import messages
+# from records2.models import Record
+# from books.models import BookInstance2, Book
+# from .models import Reservation
+# from django.core.mail import send_mail
 
-MAX_ALLOWED_RESERVATIONS = 6
+# MAX_ALLOWED_RESERVATIONS = 6
 
-def reserve(request):
-    if request.method == 'POST':
-        user_id = request.POST['user_id'] # could instead use request.user
-        book = Book.objects.get(id=request.POST['book_id'])
+# def reserve(request):
+#     if request.method == 'POST':
+#         user_id = request.POST['user_id'] # could instead use request.user
+#         book = Book.objects.get(id=request.POST['book_id'])
 
-        user_reservations = Reservation.objects.filter(user_id=user_id)
+#         user_reservations = Reservation.objects.filter(user_id=user_id)
 
-        # check to see user hasn't made more reservations than allowed
-        if user_reservations.count() >= MAX_ALLOWED_RESERVATIONS:
-            messages.error(request, 'You have the maximum number of reservations and cannot make any more')
-            return redirect(book)
-        else:
-            # check to see if user has book on loan
-            record_queryset = Record.objects.filter(user_id=user_id, book_title=book.title, date_returned=None)
-            if record_queryset:
-                messages.error(request, 'You already have this book on loan')
-                return redirect(book)
-            else:
-                # check to see if user has an active reservation for the book
-                if user_reservations.filter(book=book):
-                    messages.error(request, 'You have already reserved this book')
-                    return redirect(book)
-                else:
-                    # change status on one of the book instances
-                    instances = book.instances.all()
-                    did_change_status = False
-                    for instance in instances:
-                        if instance.status == 'a':
-                            instance.status = 'r'
-                            instance.save()
-                            did_change_status = True
-                            break
+#         # check to see user hasn't made more reservations than allowed
+#         if user_reservations.count() >= MAX_ALLOWED_RESERVATIONS:
+#             messages.error(request, 'You have the maximum number of reservations and cannot make any more')
+#             return redirect(book)
+#         else:
+#             # check to see if user has book on loan
+#             record_queryset = Record.objects.filter(user_id=user_id, book_title=book.title, date_returned=None)
+#             if record_queryset:
+#                 messages.error(request, 'You already have this book on loan')
+#                 return redirect(book)
+#             else:
+#                 # check to see if user has an active reservation for the book
+#                 if user_reservations.filter(book=book):
+#                     messages.error(request, 'You have already reserved this book')
+#                     return redirect(book)
+#                 else:
+#                     # change status on one of the book instances
+#                     instances = book.instances.all()
+#                     did_change_status = False
+#                     for instance in instances:
+#                         if instance.status == 'a':
+#                             instance.status = 'r'
+#                             instance.save()
+#                             did_change_status = True
+#                             break
 
-                    if did_change_status:
-                        reservation = Reservation(book=book, user_id=user_id, reservation_date=timezone.now(), can_collect=True, reservation_expiry=timezone.now()+timezone.timedelta(days=7))
-                        reservation.save()
-                        send_mail(
-                            f"Reservation: {reservation.book.title}",
-                            # f"Hi {request.user.username}.\n\nYour reservation for the book entitled {reservation.book.title}, is now active. A copy of the book is being held in the library and can be collected.\nNote that the reservation will expire in one week's time.\n\nRegards,\nBarham Library",
-                            f'Hello Paul!\n\nA book has been reserved on the web app.\n\n{request.user.username} has reserved the book titled "{reservation.book.title}".\n\nGo to the Staff area for further info.\n\nSincerely,\nBarham Bot',
-                            'enthuzimuzzy00@gmail.com',
-                            ['barhamlibrary@hotmail.co.uk', 'sb1664@gmail.com'],
-                            fail_silently=True
-                            )
-                    else:
-                        reservation = Reservation(book=book, user_id=user_id, reservation_date=timezone.now())
-                        reservation.save()
+#                     if did_change_status:
+#                         reservation = Reservation(book=book, user_id=user_id, reservation_date=timezone.now(), can_collect=True, reservation_expiry=timezone.now()+timezone.timedelta(days=7))
+#                         reservation.save()
+#                         send_mail(
+#                             f"Reservation: {reservation.book.title}",
+#                             # f"Hi {request.user.username}.\n\nYour reservation for the book entitled {reservation.book.title}, is now active. A copy of the book is being held in the library and can be collected.\nNote that the reservation will expire in one week's time.\n\nRegards,\nBarham Library",
+#                             f'Hello Paul!\n\nA book has been reserved on the web app.\n\n{request.user.username} has reserved the book titled "{reservation.book.title}".\n\nGo to the Staff area for further info.\n\nSincerely,\nBarham Bot',
+#                             'enthuzimuzzy00@gmail.com',
+#                             ['barhamlibrary@hotmail.co.uk', 'sb1664@gmail.com'],
+#                             fail_silently=True
+#                             )
+#                     else:
+#                         reservation = Reservation(book=book, user_id=user_id, reservation_date=timezone.now())
+#                         reservation.save()
 
-                    messages.success(request, 'You have now reserved this book')
-                    return redirect(book) # or /book/+book.id if id was a string
+#                     messages.success(request, 'You have now reserved this book')
+#                     return redirect(book) # or /book/+book.id if id was a string
--- staff/views.py
+++ staff/views.py
@@ -848,21 +848,21 @@ from django.utils import timezone
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models.functions import Lower
 
-from records2.models import Record
-from reservations.models import Reservation
+# from records2.models import Record
+# from reservations.models import Reservation
 
 MAX_ALLOWED_LOANS = 6
 
-@staff_member_required
-def index(request):
-    reservations = Reservation.objects.order_by('reservation_expiry')
-    active_records = Record.objects.filter(date_returned__isnull=True)
-    context = {
-        'reservations': reservations,
-        'active_records': active_records,
-    }
+# @staff_member_required
+# def index(request):
+#     reservations = Reservation.objects.order_by('reservation_expiry')
+#     active_records = Record.objects.filter(date_returned__isnull=True)
+#     context = {
+#         'reservations': reservations,
+#         'active_records': active_records,
+#     }
 
-    return render(request, 'staff/staff_dashboard.html', context)
+#     return render(request, 'staff/staff_dashboard.html', context)
 
 @staff_member_required
 def users(request):
@@ -894,189 +894,189 @@ def users(request):
     
     return render(request, 'staff/users.html', context)
 
-@staff_member_required
-def user(request, user_id):
-    looked_up_user = User.objects.get(id=user_id)
-    user_reservations = Reservation.objects.filter(user_id=user_id)
-    user_records = Record.objects.filter(user_id=user_id)
-    active_user_records = user_records.filter(date_returned__isnull=True)
-    active_user_records_count = active_user_records.count()
+# @staff_member_required
+# def user(request, user_id):
+#     looked_up_user = User.objects.get(id=user_id)
+#     user_reservations = Reservation.objects.filter(user_id=user_id)
+#     user_records = Record.objects.filter(user_id=user_id)
+#     active_user_records = user_records.filter(date_returned__isnull=True)
+#     active_user_records_count = active_user_records.count()
 
-    # pagination
-    paginator = Paginator(user_records, 10)
-    page = request.GET.get('page')
-    paged_records = paginator.get_page(page)
+#     # pagination
+#     paginator = Paginator(user_records, 10)
+#     page = request.GET.get('page')
+#     paged_records = paginator.get_page(page)
 
-    context = {
-        'looked_up_user': looked_up_user,
-        'user_reservations': user_reservations,
-        'user_records': paged_records,                  # paged records
-        'active_user_records': active_user_records,
-        'active_user_records_count': active_user_records_count,
-    }
-    return render(request, 'staff/user.html', context)
+#     context = {
+#         'looked_up_user': looked_up_user,
+#         'user_reservations': user_reservations,
+#         'user_records': paged_records,
+#         'active_user_records': active_user_records,
+#         'active_user_records_count': active_user_records_count,
+#     }
+#     return render(request, 'staff/user.html', context)
 
-@staff_member_required
-def loan_books(request, user_id):
+# @staff_member_required
+# def loan_books(request, user_id):
 
-    looked_up_user = User.objects.get(id=user_id)
+#     looked_up_user = User.objects.get(id=user_id)
 
-    # quick check to make sure user is verified
-    if not looked_up_user.profile.verified:
-        messages.error(request, 'User is not verified')
-        return redirect('user', looked_up_user.id)
+#     # quick check to make sure user is verified
+#     if not looked_up_user.profile.verified:
+#         messages.error(request, 'User is not verified')
+#         return redirect('user', looked_up_user.id)
 
-    # check to see if user has more active loans than is allowed
-    active_loans_count = Record.objects.filter(user_id=user_id,date_returned=None).count()
-    if active_loans_count >= MAX_ALLOWED_LOANS:
-        messages.error(request, 'User has maximum number of active loans allowed (' + str(active_loans_count) + ')')
-        return redirect('user', looked_up_user.id)
-    loans_possible_num = MAX_ALLOWED_LOANS - active_loans_count
+#     # check to see if user has more active loans than is allowed
+#     active_loans_count = Record.objects.filter(user_id=user_id,date_returned=None).count()
+#     if active_loans_count >= MAX_ALLOWED_LOANS:
+#         messages.error(request, 'User has maximum number of active loans allowed (' + str(active_loans_count) + ')')
+#         return redirect('user', looked_up_user.id)
+#     loans_possible_num = MAX_ALLOWED_LOANS - active_loans_count
 
-    # change this code to take advantage of can_collect
-    user_reservations = Reservation.objects.filter(user_id=user_id)
-    reserved_instances = set()
-    if user_reservations:
+#     # change this code to take advantage of can_collect
+#     user_reservations = Reservation.objects.filter(user_id=user_id)
+#     reserved_instances = set()
+#     if user_reservations:
 
-        for reservation in user_reservations:
+#         for reservation in user_reservations:
 
-            book_reservations = Reservation.objects.filter(book=reservation.book).order_by('reservation_date')
+#             book_reservations = Reservation.objects.filter(book=reservation.book).order_by('reservation_date')
 
-            user_placing = 0
-            for book_reservation in book_reservations:
-                user_placing += 1
-                if book_reservation.user_id == user_id:
-                    break
+#             user_placing = 0
+#             for book_reservation in book_reservations:
+#                 user_placing += 1
+#                 if book_reservation.user_id == user_id:
+#                     break
 
-            reserved_instances_for_book = BookInstance2.objects.filter(book=reservation.book, status='r')
-            number_of_reserved_instances = reserved_instances_for_book.count()
+#             reserved_instances_for_book = BookInstance2.objects.filter(book=reservation.book, status='r')
+#             number_of_reserved_instances = reserved_instances_for_book.count()
 
-            if user_placing <= number_of_reserved_instances:
-                for instance in reserved_instances_for_book:
-                    reserved_instances.add(instance)
+#             if user_placing <= number_of_reserved_instances:
+#                 for instance in reserved_instances_for_book:
+#                     reserved_instances.add(instance)
 
-    available_instances = BookInstance2.objects.filter(status='a')
+#     available_instances = BookInstance2.objects.filter(status='a')
 
-    context = {
-        'available_instances': available_instances,
-        'reserved_instances': reserved_instances,
-        'looked_up_user': looked_up_user,
-        'active_loans_count': active_loans_count,
-        'loans_possible_num': loans_possible_num,
-    }
+#     context = {
+#         'available_instances': available_instances,
+#         'reserved_instances': reserved_instances,
+#         'looked_up_user': looked_up_user,
+#         'active_loans_count': active_loans_count,
+#         'loans_possible_num': loans_possible_num,
+#     }
 
-    return render(request, 'staff/loan_books.html', context)
+#     return render(request, 'staff/loan_books.html', context)
 
-@staff_member_required
-def execute(request):
-    if request.method == 'POST':
-        user_id = request.POST['user_id']
-        user = User.objects.get(id=user_id)
+# @staff_member_required
+# def execute(request):
+#     if request.method == 'POST':
+#         user_id = request.POST['user_id']
+#         user = User.objects.get(id=user_id)
 
-        # quick check to make sure user is verified
-        if not user.profile.verified:
-            messages.error(request, 'User is not verified')
-            return redirect('user', user.id)
+#         # quick check to make sure user is verified
+#         if not user.profile.verified:
+#             messages.error(request, 'User is not verified')
+#             return redirect('user', user.id)
 
-        instances_to_be_loaned_out = set()
+#         instances_to_be_loaned_out = set()
 
-        if 'books' not in request.POST:
-            messages.error(request, 'Need to select a book, bro')
-            return redirect('loan_books', user.id)
-        else:
-            instance_ids = request.POST.getlist('books')
+#         if 'books' not in request.POST:
+#             messages.error(request, 'Need to select a book, bro')
+#             return redirect('loan_books', user.id)
+#         else:
+#             instance_ids = request.POST.getlist('books')
 
-            for an_id in instance_ids:
-                instance = get_object_or_404(BookInstance2, id=an_id)
-                instances_to_be_loaned_out.add(instance)
+#             for an_id in instance_ids:
+#                 instance = get_object_or_404(BookInstance2, id=an_id)
+#                 instances_to_be_loaned_out.add(instance)
 
-        # so now we have instances
+#         # so now we have instances
 
-        user_reservations = Reservation.objects.filter(user_id=user_id)
+#         user_reservations = Reservation.objects.filter(user_id=user_id)
 
-        active_loans_count = Record.objects.filter(user_id=user_id, date_returned__isnull=True).count()
+#         active_loans_count = Record.objects.filter(user_id=user_id, date_returned__isnull=True).count()
 
-        if len(instances_to_be_loaned_out) + active_loans_count > MAX_ALLOWED_LOANS:
-            messages.error(request, 'Too many loans selected')
-            return redirect('loan_books', user.id)
+#         if len(instances_to_be_loaned_out) + active_loans_count > MAX_ALLOWED_LOANS:
+#             messages.error(request, 'Too many loans selected')
+#             return redirect('loan_books', user.id)
 
 
 
-        if instances_to_be_loaned_out:
-            for an_instance in instances_to_be_loaned_out:
-                record = Record.objects.create(
-                    user_id=user.id,
-                    book_instance_id=an_instance.id,
-                    book_title=an_instance.book.title,
-                    date_taken_out=date.today()
-                )
-                an_instance.status = 'o'
-                an_instance.due_back = timezone.now() + timezone.timedelta(days=30)
-                an_instance.borrower = user
-                an_instance.save()
+#         if instances_to_be_loaned_out:
+#             for an_instance in instances_to_be_loaned_out:
+#                 record = Record.objects.create(
+#                     user_id=user.id,
+#                     book_instance_id=an_instance.id,
+#                     book_title=an_instance.book.title,
+#                     date_taken_out=date.today()
+#                 )
+#                 an_instance.status = 'o'
+#                 an_instance.due_back = timezone.now() + timezone.timedelta(days=30)
+#                 an_instance.borrower = user
+#                 an_instance.save()
 
-                # remove the reservation for the instance
-                if user_reservations:
-                    for reservation in user_reservations:
-                        if reservation.book.id == an_instance.book.id:
-                            reservation.delete()
-            messages.success(request, 'The books have been successfully loaned out')
-            return redirect('user', user.id)
+#                 # remove the reservation for the instance
+#                 if user_reservations:
+#                     for reservation in user_reservations:
+#                         if reservation.book.id == an_instance.book.id:
+#                             reservation.delete()
+#             messages.success(request, 'The books have been successfully loaned out')
+#             return redirect('user', user.id)
 
 # helper function for returning a book
-def return_book(record):
-    assert record.date_returned == None
-    record.date_returned = timezone.now()
-    record.save()
+# def return_book(record):
+#     assert record.date_returned == None
+#     record.date_returned = timezone.now()
+#     record.save()
 
-    instance = BookInstance2.objects.get(id=record.book_instance_id)
+#     instance = BookInstance2.objects.get(id=record.book_instance_id)
 
-    # check if there are any waiting reservations
-    # if so: change instance status to 'r' and update reservation
-    # otherwise change instance status to 'a' and remove borrower
-    waiting_reservations = Reservation.objects.filter(book=instance.book, can_collect=False).order_by('reservation_date')
-    if waiting_reservations:
-        reservation_to_change = waiting_reservations.first()
-        reservation_to_change.can_collect = True
-        reservation_to_change.reservation_expiry = timezone.now() + timezone.timedelta(days=7)
-        reservation_to_change.save()
+#     # check if there are any waiting reservations
+#     # if so: change instance status to 'r' and update reservation
+#     # otherwise change instance status to 'a' and remove borrower
+#     waiting_reservations = Reservation.objects.filter(book=instance.book, can_collect=False).order_by('reservation_date')
+#     if waiting_reservations:
+#         reservation_to_change = waiting_reservations.first()
+#         reservation_to_change.can_collect = True
+#         reservation_to_change.reservation_expiry = timezone.now() + timezone.timedelta(days=7)
+#         reservation_to_change.save()
 
-        instance.status = 'r'
-        instance.due_back = None
-        instance.save()
-    else:
-        instance.status = 'a'
-        instance.due_back = None
-        instance.borrower = None
-        instance.save()
+#         instance.status = 'r'
+#         instance.due_back = None
+#         instance.save()
+#     else:
+#         instance.status = 'a'
+#         instance.due_back = None
+#         instance.borrower = None
+#         instance.save()
 
-@staff_member_required
-def return_books(request, user_id):
-    looked_up_user = User.objects.get(id=user_id)
-    user_records = Record.objects.filter(user_id=user_id)
-    active_user_records = user_records.filter(date_returned=None)
+# @staff_member_required
+# def return_books(request, user_id):
+#     looked_up_user = User.objects.get(id=user_id)
+#     user_records = Record.objects.filter(user_id=user_id)
+#     active_user_records = user_records.filter(date_returned=None)
 
-    if active_user_records:
-        for record in active_user_records:
-            return_book(record)
+#     if active_user_records:
+#         for record in active_user_records:
+#             return_book(record)
 
-        messages.success(request, 'All books successfully returned')
-        return redirect('user', user_id)
-    else:
-        messages.error(request, 'No active books to return')
-        return redirect('user', user_id)
+#         messages.success(request, 'All books successfully returned')
+#         return redirect('user', user_id)
+#     else:
+#         messages.error(request, 'No active books to return')
+#         return redirect('user', user_id)
 
-@staff_member_required
-def return_single_book(request, record_id):
-    record = Record.objects.get(id=record_id)
+# @staff_member_required
+# def return_single_book(request, record_id):
+#     record = Record.objects.get(id=record_id)
 
-    if record.date_returned:
-        messages.error(request, 'This book has already been returned')
-        return redirect('user', record.user_id)
-    else:
-        return_book(record)
-        messages.success(request, f'{record.book_title} successfully returned')
-        return redirect('user', record.user_id)
+#     if record.date_returned:
+#         messages.error(request, 'This book has already been returned')
+#         return redirect('user', record.user_id)
+#     else:
+#         return_book(record)
+#         messages.success(request, f'{record.book_title} successfully returned')
+#         return redirect('user', record.user_id)
 
 from .forms import StaffUserEditForm#, StaffProfileEditForm
 @staff_member_required
